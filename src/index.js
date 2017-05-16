2e2600c7e0122a4b7b5639bc250b68669cf03578
Olivia Knott <olivia@oliviaknott.com>
2017-05-15 22:19:15 -0700

    stuck at lesson 29

--- src/index.js
+++ src/index.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyAlYT-szIdjwKLGNEB3BnYEdvxnQMoK--g';
 
-//Create a new component that produces html
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-const App = () => {
-    return (
-      <div>
-        <SearchBar />
-      </div>
-    );
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos});
+        });
+    }
+
+    render() {
+        return (
+          <div>
+            <SearchBar />
+            <VideoDetail video={this.state.videos[0]} />
+            <VideoList videos={this.state.videos} />
+          </div>
+        );
+    }
 }
-
-//take this component's generated html and put it on th page
 
 ReactDOM.render(<App />, document.querySelector('.container'));
